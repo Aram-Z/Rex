7709c1504723d36a065d245bd2d237ed9cb2e25e
Aram-Z <aram.zarug@gmail.com>
2026-09-04 18:28:31 +0200

    CleansingCenter_Class,cleanse(),resendAnimals(),ok

--- src/main/java/center/CleansingCenter.java
+++ src/main/java/center/CleansingCenter.java
@@ -3,17 +3,42 @@ package center;
 import animal.Animal;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CleansingCenter {
 
-    private HashMap<Animal,AdoptionCenter> animals;
+    private HashMap<Animal, AdoptionCenter> animals;
 
     public CleansingCenter() {
         this.animals = new HashMap<>();
 
 
     }
+
     void addAnimal(Animal animal, AdoptionCenter adoptionCenter) {
         animals.put(animal, adoptionCenter);
     }
+
+    public void cleanse() {
+        if (!animals.isEmpty()) {
+            for (Animal animal : animals.keySet()) {
+                animal.cleansed();
+            }
+        }
+        resendAnimals();
+
+
+    }
+
+    public void resendAnimals() {
+        for (Map.Entry<Animal, AdoptionCenter> entry : animals.entrySet()) {
+            Animal animal = entry.getKey();
+            AdoptionCenter adoptionCenter = entry.getValue();
+
+            adoptionCenter.addAnimal(animal);
+        }
+
+        animals.clear();
+    }
 }
+
